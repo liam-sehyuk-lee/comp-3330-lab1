3bd6293eeceb019e5403cdaff0ea5fc1a0f7bc79
Liam <75mcmxc@gmail.com>
2026-09-10 15:36:44 -0700

    Task 4: Fleshing Out createComponent

--- a1-liam-lee/scripts/index.js
+++ a1-liam-lee/scripts/index.js
@@ -1,4 +1,9 @@
 import createComponent from "./w1Components.js"
 
-let blogPost = createComponent("article")
-blogPost.render()
+let headerComponent = createComponent(
+    "header",
+    "<h1>Assignment 1: JavaScript Review</h1><p>Music Edition</p>",
+    { color: "slateblue", size: "2rem", columns: 1 },
+    []
+)
+headerComponent.render()
--- a1-liam-lee/scripts/w1Components.js
+++ a1-liam-lee/scripts/w1Components.js
@@ -1,8 +1,34 @@
 import "./utils/initializeStylesheet.js"
 
-let createComponent = (type) => {
+let createComponent = (
+    type = "div",
+    content = "",
+    styles = {},
+    eventsInfo = []
+) => {
     let element = document.createElement(type)
-    element.innerText = "hello world!"
+    element.classList.add("w1-component")
+
+    if (content) {
+        element.innerHTML = content
+    }
+
+    let { color = "black", size = "1rem", columns = 1 } = styles
+
+    element.style.color = color
+    element.style.fontSize = size
+
+    let gridColumns = ""
+    for (let i = 0; i < columns; i++) {
+        gridColumns = `${gridColumns}1fr `
+    }
+    element.style.gridTemplateColumns = gridColumns.trim()
+
+    if (eventsInfo && eventsInfo.length > 0) {
+        eventsInfo.forEach((event) => {
+            element.addEventListener(event.type, event.handler)
+        })
+    }
 
     let componentObject = {
         element: element,
