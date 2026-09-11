71a793a0428fd8c473c7ca6573ccce6013953ade
Liam <75mcmxc@gmail.com>
2026-09-10 20:14:19 -0700

    Task 7: Rendering the Data

--- a1-liam-lee/scripts/data.js
+++ a1-liam-lee/scripts/data.js
@@ -48,4 +48,3 @@ let artists = [
 ];
 
 export { albums, artists };
-
--- a1-liam-lee/scripts/index.js
+++ a1-liam-lee/scripts/index.js
@@ -3,20 +3,54 @@ import createComponent from "./w1Components.js"
 
 let headerComponent = createComponent(
     "header",
-    "<h1>Assignment 1: JavaScript Review</h1>",
+    "<h1>Assignment 1: JavaScript Review</h1><p>Music Edition</p>",
     { color: "slateblue", size: "2rem", columns: 1 },
     []
 )
 
-let subTextComponent = createComponent(
-    "p",
-    "Music Edition",
-    { color: "slateblue", size: "1rem", columns: 1 },
+let artistsContainer = createComponent(
+    "main",
+    "",
+    { color: "black", size: "1rem", columns: 2 },
     []
 )
 
-headerComponent.appendChildComponent(subTextComponent)
-headerComponent.render()
+artists.forEach((artist) => {
+    let { name, artistID } = artist
 
-albums
-artists
+    let artistComponent = createComponent(
+        "article",
+        `<h2>Artist Name: ${name}</h2><h3>Albums</h3>`,
+        { color: "darkorange", size: "1.5rem", columns: 1 },
+        []
+    )
+
+    let albumListComp = createComponent(
+        "dl",
+        "",
+        { color: "darkblue", size: "1rem", columns: 3 },
+        []
+    )
+
+    let artistAlbums = albums.filter((album) => album.artistID === artistID)
+
+    let albumComponents = artistAlbums.map((album) => {
+        let { name: albumName, year } = album
+        return createComponent(
+            "div",
+            `<dt>Name: ${albumName}</dt><dd>Artist: ${name}</dd><dd>Year released: ${year}</dd>`,
+            { color: "purple", size: "0.9rem", columns: 1 },
+            []
+        )
+    })
+
+    albumComponents.forEach((albumComp) => {
+        albumListComp.appendChildComponent(albumComp)
+    })
+
+    artistComponent.appendChildComponent(albumListComp)
+    artistsContainer.appendChildComponent(artistComponent)
+})
+
+headerComponent.render()
+artistsContainer.render()
